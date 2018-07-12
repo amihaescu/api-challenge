40beda7732bc6ec3b153367e6ade18c0a56183f5
Andrei Mihaescu <amihaescu@deloittece.com>
2018-07-12 17:19:41 +0300

    secured only POST, PUT, DELETE endpoints

--- src/main/java/ro/amihaescu/challenge/config/WebSecurityConfig.java
+++ src/main/java/ro/amihaescu/challenge/config/WebSecurityConfig.java
@@ -1,13 +1,20 @@
 package ro.amihaescu.challenge.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().fullyAuthenticated().and()
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/employee/**").authenticated()
+                .antMatchers(HttpMethod.PUT, "/employee/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/employee/**").authenticated()
+                .and()
                 .httpBasic().and().csrf().disable();
     }
 }
