f2a8ba67d09bf67cc2b71b1c49992555eec6b9f3
Andrei Mihaescu <amihaescu@deloittece.com>
2018-07-24 08:55:34 +0300

    Added documentation and postman list of requests.

--- readme.md
+++ readme.md
@@ -1,1 +1,37 @@
 # API Challenge
+
+This application creates a REST API that allows all CRUD operations on a employee entity.
+
+The employee entity has the following fields:
+* UUID (generated automatically)
+* E-mail
+* Full name (first and last name)
+* Birthday 
+* List of hobbies
+
+Users maybe updated as the user pleases, the only restriction being that two users cannot
+have the same email address.
+
+The solution leverages Spring Boot packages such as: `spring-boot-starter-web`, 
+`spring-boot-starter-data-jpa`, `spring-boot-starter-security`, 
+`spring-boot-starter-test`
+
+Storage of data is done using a `mysql` Docker container. The database can be started using 
+`docker-compose` command on the root of the project. `docker-compose -f docker/stack.yml up db`.
+
+Once the database is up and running, the application can also be started. The build tool used 
+is `maven`. First, run `mvn clean install` in the root of the project. This will insure all
+dependencies have been downloaded and added to the classpath. To start the application run 
+`mvn spring-boot:run`. The application will start on port `9100`. 
+
+The root context of the API is `/employee`. Further endpoints and methods can be found on the 
+Swagger UI `http://localhost:9100/swagger-ui.html`
+
+Once the application if running you can test it using `curl` or `postman`.
+
+Curl requests could be created based on Swagger documentation or the Postman collection 
+included in this repo could be used as a starting point for testing manually.  
+
+
+ 
+ 
--- src/test/java/ro/amihaescu/challenge/controller/EmployeeControllerTest.java
+++ src/test/java/ro/amihaescu/challenge/controller/EmployeeControllerTest.java
@@ -17,16 +17,12 @@ import ro.amihaescu.challenge.dto.EmployeeDTO;
 import ro.amihaescu.challenge.model.Employee;
 import ro.amihaescu.challenge.repository.EmployeeRepository;
 
-
-import java.net.HttpRetryException;
 import java.time.LocalDate;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -72,8 +68,8 @@ public class EmployeeControllerTest {
     @Test
     public void getEmployeeNoAuthentication() throws Exception {
         List<Map<String, Object>> list = restTemplate.getForObject("http://localhost:" + port + "/employee", List.class);
-        Long id = Long.valueOf((Integer)list.get(1).get("id"));
-        Map<String, Object> responseMap = restTemplate.getForObject("http://localhost:" + port + "/employee/"+id, Map.class);
+        Long id = Long.valueOf((Integer) list.get(1).get("id"));
+        Map<String, Object> responseMap = restTemplate.getForObject("http://localhost:" + port + "/employee/" + id, Map.class);
 
         assertEquals("John Doe 2", responseMap.get("fullName"));
         assertEquals("employee2@gmail.com", responseMap.get("email"));
@@ -119,15 +115,15 @@ public class EmployeeControllerTest {
     public void updateEmployeeAuthenticated() {
         List<Map<String, Object>> list = restTemplate.getForObject("http://localhost:" + port + "/employee", List.class);
         Map<String, Object> responseMap = list.get(0);
-        Long id = Long.valueOf((Integer)list.get(0).get("id"));
+        Long id = Long.valueOf((Integer) list.get(0).get("id"));
 
         HttpHeaders headers = createAuthenticationHeaders();
         EmployeeDTO employee = new EmployeeDTO(id, "employee5@gmail.com", "John Doe 4", LocalDate.of(1991, 01, 05), Collections.singletonList("Dancing"));
         HttpEntity<EmployeeDTO> requestEntity = new HttpEntity<>(employee, headers);
 
-        ResponseEntity<EmployeeDTO> responseEntity = restTemplate.exchange("http://localhost:"+port+"employee/"+id, HttpMethod.PUT, requestEntity, EmployeeDTO.class);
+        ResponseEntity<EmployeeDTO> responseEntity = restTemplate.exchange("http://localhost:" + port + "employee/" + id, HttpMethod.PUT, requestEntity, EmployeeDTO.class);
 
-        assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 
         EmployeeDTO employeeDTO = requestEntity.getBody();
         assertEquals("John Doe 4", employeeDTO.getFullName());
@@ -135,10 +131,10 @@ public class EmployeeControllerTest {
     }
 
     @Test(expected = ResourceAccessException.class)
-    public void updateEmployeeNoAuthentication(){
+    public void updateEmployeeNoAuthentication() {
         EmployeeDTO employee = new EmployeeDTO(3L, "employee3@gmail.com", "John Doe 4", LocalDate.of(1991, 01, 05), Collections.singletonList("Dancing"));
         HttpEntity<EmployeeDTO> requestEntity = new HttpEntity<>(employee);
-        restTemplate.exchange("http://localhost:"+port+"employee/3", HttpMethod.PUT, requestEntity, EmployeeDTO.class);
+        restTemplate.exchange("http://localhost:" + port + "employee/3", HttpMethod.PUT, requestEntity, EmployeeDTO.class);
     }
 
 }
